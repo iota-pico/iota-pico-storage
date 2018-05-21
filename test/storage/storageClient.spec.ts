a72b6fc7b7395a2d08826641bacb7019d1962430
Martyn Janes <martyn@obany.com>
2018-05-21 14:43:03 +0300

    Removed sinon

--- test/storage/storageClient.spec.ts
+++ test/storage/storageClient.spec.ts
@@ -3,20 +3,13 @@
  */
 import { ITransactionClient } from "@iota-pico/business/dist/interfaces/ITransactionClient";
 import * as chai from "chai";
-import * as sinon from "sinon";
 import { StorageClient } from "../../src/storage/storageClient";
 
 describe("StorageClient", () => {
     let transactionClientStub: ITransactionClient;
-    let sandbox: sinon.SinonSandbox;
 
     beforeEach(() => {
-        sandbox = sinon.createSandbox();
         transactionClientStub = <ITransactionClient>{};
-    });
-
-    afterEach(() => {
-        sandbox.restore();
     });
 
     it("can be created", () => {
